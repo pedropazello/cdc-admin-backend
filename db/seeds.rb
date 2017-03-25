503e9a74b80cd4f5fc741631df78f64e13268d96
Pedro Pazello <email@pedropazello.com>
2017-03-25 11:20:37 -0300

    Adiciona a gem Faker e gera vários autores pelo seeds.rb para servir o cliente via API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times.each do |_|
+  Autor.create(
+    nome: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: '-secret-'
+  )
+end
